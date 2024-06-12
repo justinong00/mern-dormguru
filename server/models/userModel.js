c45e30dbcd822d925b9c6522b6359d1d8d6d1e4a
“justinong00” <justinong00@gmail.com>
2024-06-13 02:51:51 +0800

    configured userModel

---
+++ server/models/userModel.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+// Create a new Mongoose schema
+const Schema = mongoose.Schema;
+
+// Defining the document structure (Schema)
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+  },
+  { 
+    //The timestamps option tells mongoose to assign createdAt and updatedAt fields to your schema
+    timestamps: true,
+  }
+);
+
+/** Create and export the database model based on the schema.
+ * 
+ * 'User' will be imported in other files to interact with the database collection
+ * Mongoose automatically pluralizes 'User' to 'users' for the collection name
+ */
+export default mongoose.model('User', userSchema);
