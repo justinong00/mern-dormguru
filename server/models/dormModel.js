b1333f392618bc46db358dd59c6f514e0da98978
“justinong00” <justinong00@gmail.com>
2024-06-29 18:32:45 +0800

    configured Movie APIs

---
+++ server/models/dormModel.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+
+// Create a new Mongoose schema
+const Schema = mongoose.Schema;
+
+// Defining the document structure (Schema)
+const dormSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    parentUniversity: {
+      type: String,
+      required: true,
+    },
+    roomsOffered: {
+      type: [],
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    establishedYear: {
+      type: Number,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    coverPhotos: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    //The timestamps option tells mongoose to assign createdAt and updatedAt fields to your schema
+    timestamps: true,
+  }
+);
+
+/** Create and export the database model based on the schema.
+ *
+ * 'Dorm' will be imported in other files to interact with the database collection
+ * Mongoose automatically pluralizes 'Dorm' to 'dorms' for the collection name
+ */
+export default mongoose.model('Dorm', dormSchema);
