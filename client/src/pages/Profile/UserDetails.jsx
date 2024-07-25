3cf87ef1aec42ded32418c6a307bdd71a53b3020
“justinong00” <justinong00@gmail.com>
2024-07-26 07:11:08 +0800

    Added profile pic and verified student icon to header (still need to fix for non verified and admin)

--- client/src/pages/Profile/UserDetails.jsx
+++ client/src/pages/Profile/UserDetails.jsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Form, Input, message } from "antd";
+import { Button, Checkbox, Form, Input, message, Select } from "antd";
 import React, { useState } from "react";
 import { validationRules } from "../../helpers/index.js";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,12 +6,15 @@ import { setUsers } from "../../redux/usersSlice.js";
 import { setLoading } from "../../redux/loadersSlice.js";
 import { UpdateUser } from "../../apis/users.js";
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { countryOptions } from "../../helpers/countryOptions.jsx";
+import ImageUpload, { customValidateFileList } from "../../components/ImageUpload.jsx";
 
 function UserDetails() {
   const { user } = useSelector((state) => state.users); // Get the users state from the Redux store
   const dispatch = useDispatch();
   const [form] = Form.useForm(); // Create a form instance using Ant Design's useForm hook
   const [updatePassword, setUpdatePassword] = useState(false); // State to toggle password fields
+  const [fileList, setFileList] = useState([]); // State to store the uploaded file list
 
   const onFinish = async (values) => {
     try {
@@ -45,7 +48,12 @@ function UserDetails() {
         className="flex w-full max-w-md flex-col gap-y-4"
         onFinish={onFinish}
         form={form}
-        initialValues={{ name: user.name, email: user.email }}
+        initialValues={{
+          name: user.name,
+          email: user.email,
+          country: user.country,
+          profilePicture: user.profilePicture,
+        }}
       >
         {/* Name */}
         <Form.Item label="Name" name="name" rules={validationRules["name"]}>
@@ -55,6 +63,42 @@ function UserDetails() {
         {/* Email */}
         <Form.Item label="Email" name="email" rules={validationRules["email"]}>
           <Input type="email" />
+        </Form.Item>
+
+        {/* Country */}
+        <Form.Item label="Country" name="country" rules={validationRules["country"]}>
+          <Select
+            showSearch
+            placeholder="Select a country"
+            // This allows the Select component to use the labelForSearch property for filtering options based on user input.
+            optionFilterProp="labelForSearch"
+            options={countryOptions}
+            onChange={(value) => form.setFieldsValue({ country: value })}
+            // Used a custom filterOption function to filter the options based on labelForSearch.
+            filterOption={(input, option) =>
+              option.labelForSearch.toLowerCase().includes(input.toLowerCase())
+            }
+          />
+        </Form.Item>
+
+        {/* Profile Picture */}
+        <Form.Item
+          label="Profile Picture"
+          name="profilePicture"
+          rules={[
+            {
+              required: true,
+              validator: (_, value) => customValidateFileList(fileList, value),
+            },
+          ]}
+        >
+          <ImageUpload
+            fileList={fileList}
+            setFileList={setFileList}
+            selectedItem={user}
+            form={form}
+            fieldName="profilePicture"
+          />
         </Form.Item>
 
         {/* Update Password Checkbox */}
