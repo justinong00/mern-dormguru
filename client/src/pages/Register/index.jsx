00ffcfd8eba4e624c8355eb59a433cc1e0f4631e
“justinong00” <justinong00@gmail.com>
2024-07-27 01:06:54 +0800

    Added Verified Student notice in Registration page

--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -9,6 +9,7 @@ import { setLoading } from "../../redux/loadersSlice.js";
 import { countryOptions } from "../../helpers/countryOptions.jsx";
 import { AddProfilePicture } from "../../apis/images.js";
 import ImageUpload, { customValidateFileList } from "../../components/ImageUpload.jsx";
+import { FaUserGraduate } from "react-icons/fa";
 
 /** Register component for user registration. *
  *
@@ -83,9 +84,21 @@ function Register() {
 
       {/* Form section */}
       <div className="flex items-center justify-center">
-        <div className="w-[400px]">
+        <div className="w-4/6">
           <h1 className="my-5 mb-2 text-2xl">Register Your Account</h1>
           <hr />
+          <div className="border-primary mt-3 rounded-xl border-x-0 border-y-2 border-b-0 border-solid bg-gray-100 p-6 shadow-md">
+            <div className="flex flex-col items-center gap-2 sm:justify-between lg:flex-row">
+              <div className="text-center text-xs lg:w-6/12 lg:text-left xl:w-7/12 xl:text-sm 2xl:w-8/12">
+                We want DormGuru to be a trusted platform. Sign up with your school email to leave
+                reviews and earn this badge:
+              </div>
+              <div className="text-xxs flex items-center gap-x-2 rounded-md border bg-black px-2 py-1 text-white xl:text-xs">
+                <span>Verified Student</span>
+                <FaUserGraduate />
+              </div>
+            </div>
+          </div>
           <Form
             form={form}
             layout="vertical"
